06b7c3b81ac6014bd7728da32204542bd2b593c9
olbo81 <olbo81@mail.ru>
2020-11-26 23:02:56 +0500

    Всё заработало

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,19 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat beanCat = applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Бины ХЕЛЛО одинаковые? " + (bean==bean2));
+        System.out.println("Бины САТ одинаковые? " + (beanCat==beanCat2));
+
     }
 }
